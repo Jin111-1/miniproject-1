aab3620af5c48a405244fa0e268362423f0df5f2
Jin react app <Passawit14@gmail.com>
2025-03-17 13:56:46 +0700

    feat:footer deving

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <footer className="w-[1440px] h-[144px] flex justify-between pt-[60px] pr-[120px] pb-[60px] pl-[120px]">
-
+            
         </footer>
     )
 }
